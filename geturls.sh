cde7267d74d4e83dd6115a3daedccee8efe7c4a8
Simon Newhouse <simon.newhouse@gmail.com>
2018-05-03 21:52:34 +0100

    Update geturls.sh

--- geturls.sh
+++ geturls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get whole website html files only !
-wget --mirror -A "*.html" www.global-sets.com
+wget --mirror -A "*.html" -nc www.global-sets.com
 
 # code to pull links from html
 grep -r zipp ./www.global-sets.com/*| grep href | grep -o '".*"' | sed 's/"//g'> urls.txt
